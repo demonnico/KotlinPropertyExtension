85d34a0ad9d9e97790b77af94693d596fcb8dd6a
Nicholas Tau <demon42111915@gmail.com>
2018-03-28 23:23:11 +0800

    Update PropertyExtension.kt
    make sure to support any object

--- PropertyExtension.kt
+++ PropertyExtension.kt
@@ -6,7 +6,11 @@ private val Any.variablesMap by lazy {
 @Suppress("UNCHECKED_CAST")
 class PropertyExtensionDelegation<out T>{
     operator fun getValue(thisRef: Any?, property: KProperty<*>) : T = (thisRef?.variablesMap?.get(property.toString())) as T
-    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String){
-        thisRef?.variablesMap?.set(property.toString(), value)
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Any?){
+        if (value!=null){
+            thisRef?.variablesMap?.set(property.toString(), value)
+        }else{
+            thisRef?.variablesMap?.remove(property.toString())
+        }
     }
 }
